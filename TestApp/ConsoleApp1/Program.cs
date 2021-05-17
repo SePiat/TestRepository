93ec066d48e6d8f59d86edb16a5b18cbd93ac786
Sergey Piatsel <SePiatsel@gmail.com>
2021-05-17 14:04:54 +0300

    NewLineInBranchMaster2

--- TestApp/ConsoleApp1/Program.cs
+++ TestApp/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("RepearBranch");
             Console.WriteLine("NewLineInBranchMaster");
+            Console.WriteLine("NewLineInBranchMaster2");
 
         }
     }
