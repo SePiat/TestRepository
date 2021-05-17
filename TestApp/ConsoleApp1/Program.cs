1b12affdbe685454f405d9ec454c918a7b71dc50
Siarhei Piatselski <Siarhei_Piatselski@epam.com>
2021-05-17 12:42:13 +0300

    FirstCommitToSecondBranch

--- TestApp/ConsoleApp1/Program.cs
+++ TestApp/ConsoleApp1/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
